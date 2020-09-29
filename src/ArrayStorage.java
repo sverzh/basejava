da81e3fd983516b84fc89f0e3bdc791dea673778
Andrei Sverzh <sverzh@tut.by>
2020-09-29 23:24:35 +0300

    finished HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+import java.util.ArrayList;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume get = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) get = storage[i];
+        }
+        if (get == null) System.out.println("Резюме c uuid " + uuid + " не найдено");
+
+        return get;
+
+
     }
 
     void delete(String uuid) {
+        int deleted = size;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) deleted = i;
+        }
+        for (int j = deleted; j < size - 1; j++) {
+            storage[j] = storage[j + 1];
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] filled = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            filled[i] = storage[i];
+        }
+        return filled;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
